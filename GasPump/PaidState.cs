16cfc79550928655d47154ba1645101d4259ad6f
aviverit <andrew.viverito@gmail.com>
2017-04-14 02:57:24 -0500

    v.0.2.1 "All concrete states introduced."

---
+++ GasPump/PaidState.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasPump
+{
+    class PaidState : State
+    {
+        public override void approved()
+        {
+
+        }
+
+        public override void cancel()
+        {
+
+        }
+
+        public override void noReceipt()
+        {
+
+        }
+
+        public override void payCash(int c)
+        {
+
+        }
+
+        public override void payCredit()
+        {
+
+        }
+
+        public override void premium()
+        {
+
+        }
+
+        public override void pumpGallon()
+        {
+
+        }
+
+        public override void pumpLiter()
+        {
+
+        }
+
+        public override void receipt()
+        {
+
+        }
+
+        public override void regular()
+        {
+
+        }
+
+        public override void Reject()
+        {
+
+        }
+
+        public override void start()
+        {
+
+        }
+
+        public override void startPump()
+        {
+
+        }
+
+        public override void stopPump()
+        {
+
+        }
+
+        public override void super()
+        {
+
+        }
+    }
+}
