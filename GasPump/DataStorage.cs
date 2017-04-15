0ef1234294ba83ec362f1b7502d6ae4966c4120e
aviverit <andrew.viverito@gmail.com>
2017-04-15 01:39:05 -0500

    v.0.5.4 "Data Storage expanded"

--- GasPump/DataStorage.cs
+++ GasPump/DataStorage.cs
@@ -8,5 +8,36 @@ namespace GasPump
 {
     class DataStorage
     {
+        float a1, b1;
+        int a2, b2, c2, cash;
+
+        public void set1A(float a) {
+            a1 = a;
+        }
+
+        public void set1B(float b)
+        {
+            b1 = b;
+        }
+
+        public void set2A(int a)
+        {
+            a2 = a;
+        }
+
+        public void set2B(int b)
+        {
+            b2 = b;
+        }
+
+        public void set2C(int c)
+        {
+            c2 = c;
+        }
+
+        public void set2Cash(int c)
+        {
+            cash = c;
+        }
     }
 }
