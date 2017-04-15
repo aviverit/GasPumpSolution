1d1925ee599036ddd7133e3c403794830d585776
aviverit <andrew.viverito@gmail.com>
2017-04-15 01:26:36 -0500

    v.0.5.2 "IP Done"

--- GasPump/GasPump2.cs
+++ GasPump/GasPump2.cs
@@ -8,5 +8,80 @@ namespace GasPump
 {
     class GasPump2
     {
+        MDEFSM mEP;
+        DataStorage dSP;
+
+        GasPump1()
+        {
+
+        }
+
+        public void activate(int a, int b, int c)
+        {
+            if ((a > 0) && (b > 0) && (c>0))
+            {
+                dSP.set2A(a);
+                dSP.set2B(b);
+                dSP.set2C(c);
+                mEP.activate();
+            }
+        }
+
+        public void start()
+        {
+            mEP.start();
+        }
+
+        public void payCash(int c)
+        {
+            dSP.set2Cash(c);
+            mEP.payType(0);
+        }
+
+        public void cancel()
+        {
+            mEP.cancel();
+        }
+
+        public void premium()
+        {
+            mEP.selectGas(2);
+        }
+
+        public void super()
+        {
+            mEP.selectGas(1);
+        }
+
+        public void regular()
+        {
+            mEP.selectGas(0);
+        }
+
+        public void startPump()
+        {
+            mEP.startPump();
+        }
+
+        public void pumpLiter()
+        {
+            if (dSP.get2Cash() < (dSP.get2L() + 1) * dSP.get2Price()) {
+                mEP.stopPump();
+            }
+            mEP.pumpOnce();
+        }
+
+        public void stop()
+        {
+            mEP.stopPump();
+        }
+
+        public void receipt() {
+            mEP.receipt();
+        }
+
+        public void noReceipt() {
+            mEP.noReceipt();
+        }
     }
 }
