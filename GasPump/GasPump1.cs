cd79baee9128a7183aa5dbe911b94aadadb270ac
aviverit <andrew.viverito@gmail.com>
2017-04-15 01:10:14 -0500

    v.0.5.1 "GP1 Done"

--- GasPump/GasPump1.cs
+++ GasPump/GasPump1.cs
@@ -8,9 +8,62 @@ namespace GasPump
 {
     class GasPump1
     {
-        GasPump1() {
-            MDEFSM mEP;
-            DataStorage dSP;
+        MDEFSM mEP;
+        DataStorage dSP;
+
+        GasPump1()
+        {
+            
+        }
+
+        public void activate(float a, float b) {
+            if ((a > 0) && (b > 0)) {
+                dSP.set1A(a);
+                dSP.set1B(b);
+                mEP.activate();
+            }
+        }
+
+        public void start() {
+            mEP.start();
+        }
+
+        public void payCredit() {
+            mEP.payType(1);
+        }
+
+        public void reject() {
+            mEP.reject();
+        }
+
+        public void cancel() {
+            mEP.cancel();
+        }
+
+        public void approved() {
+            mEP.approved();
+        }
+
+        public void super() {
+            mEP.selectGas(1);
+        }
+
+        public void regular() {
+            mEP.selectGas(0);
+        }
+
+        public void startPump() {
+            mEP.startPump();
+        }
+
+        public void pumpGallon() {
+            mEP.pumpOnce();
+        }
+
+        public void stopPump() {
+            mEP.stopPump();
+            mEP.receipt();
         }
     }
 }
+
