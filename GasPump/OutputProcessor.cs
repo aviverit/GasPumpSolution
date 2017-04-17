f1956401674b1b86495f136e1a75ce913d53d623
Andrew Viverito <andrew.viverito@gmail.com>
2017-04-16 19:04:53 -0500

    v.0.6.8 "OutputProcessor more fleshed out (unfinished). "

--- GasPump/OutputProcessor.cs
+++ GasPump/OutputProcessor.cs
@@ -8,5 +8,55 @@ namespace GasPump
 {
     class OutputProcessor
     {
+
+        public void payMsg(){
+            Console.WriteLine("Please insert payment. ");
+        }    
+
+        public void storeCash(){
+
+        }
+
+        public void displayMenu(){
+            Console.WriteLine("Menu");
+        }
+
+        public void rejectMsg(){
+            Console.WriteLine("Sorry, but your card was declined. ");
+        }
+
+        public void setPrice(int gT){
+
+        }
+
+        public void setInitialValues(){
+
+        }
+
+        public void readyMsg(){
+            Console.WriteLine("Pump is now ready. Please insert into tank and begin fueling. ");
+        }
+
+        public void pumpOnce(){}
+
+        public void gasPumpedMsg(){
+            Console.WriteLine(gQ+" "+gT+" have been pumped.");
+        }
+
+        public void stopMsg(){
+            Console.WriteLine("Finished Fueling. Thank you for choosing us.");
+        }
+
+        public void returnCash(){
+
+        }
+
+        public void receipt(){
+
+        }
+
+        public void noReceipt(){
+
+        }
     }
 }
