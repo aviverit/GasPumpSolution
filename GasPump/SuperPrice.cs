2630293153522eb684acb6472dde3600e9ce5ac6
aviverit <andrew.viverito@gmail.com>
2017-04-19 21:31:26 -0500

    v.0.7.2 "SuperPrice Added"

---
+++ GasPump/SuperPrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasPump
+{
+    class SuperPrice : PriceStrategy
+    {
+        public override void setPrice()
+        {
+
+        }
+    }
+}
